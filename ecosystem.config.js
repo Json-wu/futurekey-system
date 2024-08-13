526041e9fb306d718492ae4a9f03abeb13051c65
yongqiangwu <yongqiangwu1@163.com>
2024-08-14 00:01:31 +0800

    add pm2 log.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-    apps : [{
-      name: 'classroom',
-      mode: 'fork',
-      namespace:'myapp',
-      script: './src/app.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-    }]
-  };
+  apps: [{
+    name: 'classroom',
+    exec_mode: 'fork',
+    namespace: 'myapp',
+    script: './src/app.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    error_file: "err.log",
+    out_file: "out.log",
+    merge_logs: true,
+    log_date_format: "YYYY-MM-DD HH:mm Z"
+  }]
+};
