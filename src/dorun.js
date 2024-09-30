a851e64ff3797064c21b91e9eb08344ed08da5c4
Json-wu <yongqiangwu1@163.com>
2024-09-30 08:44:06 +0800

    edit teacher' page 2

--- src/dorun.js
+++ src/dorun.js
@@ -4,7 +4,10 @@ const db = require('./libs/db');
 /**
  * 创建表
  */
-db.run("DROP TABLE IF EXISTS class_his;");
-console.log('已清空class_his');
-db.run("DROP TABLE IF EXISTS courses;");
-console.log('已清空courses');
+// db.run("DROP TABLE IF EXISTS class_his;");
+// console.log('已清空class_his');
+//db.run("DROP TABLE IF EXISTS courses;");
+//console.log('已清空courses');
+db.run("DROP TABLE IF EXISTS student_detail;");
+console.log('已清空student_detail');
+
