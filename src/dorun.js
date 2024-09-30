cb5dff4ae2d1946e66536a80dc18e3c4332f76d3
jason <yongqiangwu1@163com>
2024-09-30 18:20:04 +0800

    edit teacher's page

--- src/dorun.js
+++ src/dorun.js
@@ -1,13 +1,14 @@
 // db.js
-const db = require('./libs/db');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('logs.db');
 
 /**
  * 创建表
  */
 // db.run("DROP TABLE IF EXISTS class_his;");
 // console.log('已清空class_his');
-//db.run("DROP TABLE IF EXISTS courses;");
-//console.log('已清空courses');
+// db.run("DROP TABLE IF EXISTS courses;");
+// console.log('已清空courses');
 db.run("DROP TABLE IF EXISTS student_detail;");
 console.log('已清空student_detail');
 
