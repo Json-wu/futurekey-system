3dc86d8bebe925d6abaf6e809618bdc0cf8567a2
Json-wu <yongqiangwu1@163.com>
2024-10-23 02:50:57 +0800

    edit dorun

--- src/dorun.js
+++ src/dorun.js
@@ -13,7 +13,7 @@ const db = new sqlite3.Database('logs.db');
 // console.log('已清空student_detail');
 // db.run("DROP TABLE IF EXISTS leave;");
 // console.log('已清空leave');
-db.run("DELETE from  message_info where url='';");
+db.run("DELETE from  message_info where 1=1;");
 console.log('已清空message_info');
 
 // db.run("update courses set attend='0' where value2='1';");
